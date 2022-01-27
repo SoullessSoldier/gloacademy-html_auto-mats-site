9f10287b446d42fc136b7ecced011f9f82b74f3c
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2022-01-27 23:28:59 +0200

    add Day3

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 3,
+    loop: true,
+    speed: 400,
+    spaceBetween: 70,
+    centeredSlides: true,
+    slideToClickedSlide: true,
+    toggle: true,
+    breakpoints:{
+        320: {
+            slidesPerView: 1,
+        },
+        1400: {
+            slidesPerView: 3
+        }
+    }
+});
